bd8e38342711b6ec6efb8ed2a66a52537ffb7fba
Mohamed Abdelghani <mabdelghani2010@hotmail.com>
2020-01-15 01:47:56 +0200

    initial commit

---
+++ Converter.Designer.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Excel2JsonConverter
+{
+    partial class Converter
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+   
+
+            this.Text = "Excel to Json Converter";
+            this.btnBrowse = new System.Windows.Forms.Button();
+
+            this.btnBrowse.Location = new System.Drawing.Point(104, 40);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(162, 61);
+            this.btnBrowse.TabIndex = 0;
+            this.btnBrowse.Text = "Browse For Source File";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+
+            this.components = new System.ComponentModel.Container();
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(400, 200);
+            this.Controls.Add(this.btnBrowse);
+
+        }
+
+        
+
+        #endregion
+        private System.Windows.Forms.Button btnBrowse;
+
+    }
+}
+
---
+++ Converter.cs
@@ -0,0 +1,70 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Excel2JsonConverter
+{
+    public partial class Converter : Form
+    {
+        public Converter()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            using OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.InitialDirectory = "c:\\";
+            openFileDialog.Filter = "Excel Files|*.xls;*.xlsx;*.xlsm";
+            openFileDialog.FilterIndex = 2;
+            openFileDialog.RestoreDirectory = true;
+
+            if (openFileDialog.ShowDialog() != DialogResult.OK)
+            {
+                return;
+            }
+
+            var convertedFiles = new List<string>();
+            var sourcePath = openFileDialog.FileName;
+            var fileStream = openFileDialog.OpenFile();
+
+            using var package = new ExcelPackage(fileStream);
+            foreach (var worksheet in package.Workbook.Worksheets)
+            {
+                var data = new List<JObject>();
+
+                var rowCount = worksheet.Dimension.Rows;
+                var columnCount = worksheet.Dimension.Columns;
+
+                var headers = new List<string>();
+
+                for (int col = 1; col <= columnCount; col++)
+                {
+                    headers.Add(worksheet.Cells[1, col].Value.ToString().Trim());
+                }
+
+                for (int row = 2; row <= rowCount; row++)
+                {
+                    dynamic obj = new JObject();
+
+                    for (int col = 1; col <= columnCount; col++)
+                    {
+                        obj[headers[col - 1]] = worksheet.Cells[row, col].Value?.ToString().Trim();
+                    }
+                    data.Add(obj);
+                }
+
+                var destPath = $"{sourcePath.Substring(0, sourcePath.LastIndexOf('.'))}.json";
+                using StreamWriter file = File.CreateText(destPath);
+                new JsonSerializer().Serialize(file, data);
+                convertedFiles.Add(destPath);
+            }
+            MessageBox.Show($"Converted Files: {string.Join('\n', convertedFiles)}");
+        }
+    }
+}
